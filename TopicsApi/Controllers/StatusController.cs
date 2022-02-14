6439a7b19cfe45c48311a4b7416fa25870b907d8
Murphy-Joe <ibidnsell@yahoo.com>
2022-02-14 12:34:31 -0500

    Add project files.

---
+++ TopicsApi/Controllers/StatusController.cs
@@ -0,0 +1,11 @@
+﻿namespace TopicsApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("status/oncalldev")]
+
+    public ActionResult GetOnCallDev()
+    {
+        return Ok(); // this returns a 200 status response
+    }
+}
---
+++ TopicsApi/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// configuring backend services ...
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// building the actual application ...
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// culminates in this ...
+app.Run(); // basically in a forever loop, listening for http requests
